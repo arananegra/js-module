d2ca8f4ca5a3c48be456e5843b32f06d948e5a88
Alvaro <programaciongsl4@gmail.com>
2019-04-17 12:28:32 +0200

    Working on problems

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-let x: number = 10;
+//1. Array Operations
+const head = (array: Array<any>) => {
+    let [firstElement] = array;
+    return firstElement;
+}
 
-console.log(x)
+const tail = (array: Array<any>): Array<any> => {
+    let [, ...others] = array;
+    return others;
+}
+
+const init = (array: Array<any>): Array<any> => {
+    return array.slice(0, -1);
+}
+
+const last = (array: Array<any>): Array<any> => {
+    return array.slice(-1);
+}
+
+//2. Concat
+const concat = (a: Array<any>, b: Array<any>): Array<any> => {
+    
+}
---
+++ src/test.js
@@ -0,0 +1,7 @@
+const student = {
+    name: "Evan",
+    surname: "Smith",
+    country: "USA",
+};
+const getName = ({ name }) => name;
+console.log(getName(student)); 
