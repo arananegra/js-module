6f1465c4c149682f575fb0faa70222a57efd68c6
Alvaro <programaciongsl4@gmail.com>
2019-04-17 13:07:14 +0200

    Keep going

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,61 @@
 //1. Array Operations
-const head = (array: Array<any>) => {
+const head = (array) => {
     let [firstElement] = array;
     return firstElement;
 }
 
-const tail = (array: Array<any>): Array<any> => {
+//console.log(head([1, 2, 3]));
+
+const tail = (array) => {
     let [, ...others] = array;
     return others;
 }
 
-const init = (array: Array<any>): Array<any> => {
+//console.log(tail([1, 2, 3]));
+
+const init = (array) => {
     return array.slice(0, -1);
 }
 
-const last = (array: Array<any>): Array<any> => {
+//console.log(init([1, 2, 3]));
+
+const last = (array) => {
     return array.slice(-1);
 }
 
+//console.log(last([1, 2, 3]));
+
 //2. Concat
-const concat = (a: Array<any>, b: Array<any>): Array<any> => {
-    
+const concat = (a, b) => {
+    return [...a, ...b];
 }
+
+//console.log(concat([1, 2, 3], [4, 5, 6]));
+
+const concatMultiple = (...a) => {
+    return a.reduce((acc, current) => [...acc, ...current], []);
+}
+
+//console.log(concatMultiple([1, 2, 3], [4, 5, 6]));
+
+//3. Clone merge
+function clone(source) {
+    return { ...source };
+}
+
+//console.log(clone({a: "coso", b: "coso2"}));
+
+function merge(source, target) {
+    return { ...clone(target), ...clone(source) };
+}
+const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+const b = { name: "Luisa", age: 31, married: true };
+
+//console.log(merge(a, b));
+
+//4. Read books
+
+
+
+
+
